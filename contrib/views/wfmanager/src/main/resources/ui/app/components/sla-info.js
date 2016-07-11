479b4551f47fcffdd6057ff505ea2789bd75eddc
padmapriyanitt <padmapriyanitt@gmail.com>
2016-07-11 17:54:11 +0530

    SLA changes

--- contrib/views/wfmanager/src/main/resources/ui/app/components/sla-info.js
+++ contrib/views/wfmanager/src/main/resources/ui/app/components/sla-info.js
@@ -15,15 +15,72 @@
  *    limitations under the License.
  */
 import Ember from 'ember';
+import EmberValidations from 'ember-validations';
 
-export default Ember.Component.extend({
+export default Ember.Component.extend(EmberValidations, {
+  alertEvents: Ember.A([]),
+  timeUnitOptions : Ember.A([]),
+  initialize : function(){
+    this.set('alertEvents', Ember.A([]));
+    this.get('alertEvents').pushObject({eventType:'start_miss', alertEnabled:false, displayName :'Start Miss'});
+    this.get('alertEvents').pushObject({eventType:'end_miss', alertEnabled:false, displayName : 'End Miss'});
+    this.get('alertEvents').pushObject({eventType:'duration_miss', alertEnabled:false, displayName:'Duration Miss'});
+
+    Ember.addObserver(this, 'alertEvents.@each.alertEnabled', this, this.alertEventsObserver);
+
+    this.set('timeUnitOptions',Ember.A([]));
+    this.get('timeUnitOptions').pushObject({value:'',displayName:'Select'});
+    this.get('timeUnitOptions').pushObject({value:'MINUTES',displayName:'Minutes'});
+    this.get('timeUnitOptions').pushObject({value:'HOURS',displayName:'Hours'});
+    this.get('timeUnitOptions').pushObject({value:'DAYS',displayName:'Days'});
+
+    if(this.get('slaInfo.alertEvents')){
+      var alertsFor = this.get('slaInfo.alertEvents').split(",");
+      alertsFor.forEach((alert)=>{
+        Ember.set(this.get('alertEvents').findBy('eventType', alert),'alertEnabled', true);
+      });
+    }
+  }.on('init'),
+  alertEventsObserver : function(){
+    var alerts = this.get('alertEvents').filterBy('alertEnabled',true).mapBy('eventType');
+    this.set('slaInfo.alertEvents', alerts.join());
+  },
+  onDestroy : function(){
+    Ember.removeObserver(this, 'alertEvents.@each.alertEnabled', this, this.alertEventsObserver);
+  }.on('willDestroyElement'),
   elementsInserted : function() {
     this.$('#nominalTime').datetimepicker({
       useCurrent: false,
       showClose : true,
       defaultDate : this.get('slaInfo.nominalTime')
     });
+    if(this.get('type') === 'action'){
+      this.sendAction('register','slaInfo', this);
+    }
   }.on('didInsertElement'),
+
+  shouldEnd : Ember.computed.alias('slaInfo.shouldEnd'),
+  canValidate : Ember.computed.alias('slaEnabled'),
+  validations : {
+     'slaInfo.nominalTime': {
+       'if': 'canValidate',
+       presence: {
+         'message' : 'You need to provide a value for Nominal time',
+       }
+     },
+     'shouldEnd.time': {
+       'if': 'canValidate',
+       presence: {
+         'message' : 'You need to provide a value for Should End',
+       }
+     },
+     'shouldEnd.unit': {
+       'if': 'canValidate',
+       presence: {
+         'message' : 'You need to select a time unit for Should End',
+       }
+      }
+  },
   slaObserver : Ember.observer('slaEnabled',function(){
     if(this.get('slaEnabled')){
       this.$('#slaCollapse').collapse('show');
